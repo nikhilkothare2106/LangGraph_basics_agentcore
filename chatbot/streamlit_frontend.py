46813426f3b7bbb3f82dad0505f80cadba21b0a5
EC2 Default User <ec2-user@ip-10-0-1-191.ap-south-1.compute.internal>
2026-08-12 11:15:55 +0000

    some change

--- chatbot/streamlit_frontend.py
+++ chatbot/streamlit_frontend.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langgraph_backend import chatbot
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessageChunk
 from langchain_core.runnables import RunnableConfig
 
 # st.session_state -> dict -> 
@@ -24,12 +24,36 @@ if user_input:
     # first add the message to message_history
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
-        st.text(user_input)
+        st.markdown(user_input)
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
-    
-    ai_message = response['messages'][-1].content
+    # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    # ai_message = response['messages'][-1].content
+
+
     # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
-    with st.chat_message('assistant'):
-        st.text(ai_message)
+    # st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    # with st.chat_message('assistant'):
+    #     st.markdown(ai_message)
+
+
+    # streaming response
+    
+    ai_message = ""
+    
+    with st.chat_message("assistant"):
+    
+        def generate():
+            for message_chunk, metadata in chatbot.stream(
+                {"messages": [HumanMessage(content=user_input)]},
+                config=CONFIG,
+                stream_mode="messages",
+            ):
+                if isinstance(message_chunk, AIMessageChunk):
+                    yield str(message_chunk.content)
+    
+        ai_message = st.write_stream(generate())
+    
+    st.session_state["message_history"].append({
+        "role": "assistant",
+        "content": ai_message
+    })
